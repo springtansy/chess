7fa1a2884fd650d1fdcb21735720c685f1e824e3
springtansy <261604151+springtansy@users.noreply.github.com>
2026-08-11 20:20:20 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const board = document.getElementById("board");
+
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+
+        const square = document.createElement("div");
+
+        square.classList.add("square");
+
+        if ((row + col) % 2 === 0) {
+            square.classList.add("light");
+        } else {
+            square.classList.add("dark");
+        }
+
+        board.appendChild(square);
+    }
+}
