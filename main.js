447bfa2712db98b8904fd9a18a8fc562927d0f61
springtansy <261604151+springtansy@users.noreply.github.com>
2026-08-11 20:29:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const board = document.getElementById("board");
 
+const pieces = {
+    wK: "pieces/whiteking.svg",
+    wQ: "pieces/whitequeen.svg",
+    wR: "pieces/whiterook.svg",
+    wB: "pieces/whitebishop.svg",
+    wN: "pieces/whiteknight.svg",
+    wP: "pieces/whitepawn.svg",
+
+    bK: "pieces/blackking.svg",
+    bQ: "pieces/blackqueen.svg",
+    bR: "pieces/blackrook.svg",
+    bB: "pieces/blackbishop.svg",
+    bN: "pieces/blackknight.svg",
+    bP: "pieces/blackpawn.svg"
+};
+
+const startingPosition = [
+    ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+    ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+    ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+];
+
 for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
 
@@ -11,6 +38,17 @@ for (let row = 0; row < 8; row++) {
             square.classList.add("light");
         } else {
             square.classList.add("dark");
+        }
+
+        const piece = startingPosition[row][col];
+
+        if (piece) {
+            const image = document.createElement("img");
+
+            image.src = pieces[piece];
+            image.alt = piece;
+
+            square.appendChild(image);
         }
 
         board.appendChild(square);
