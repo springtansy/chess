387f8c8467fa85199e7ff08f79589b7bf612638e
springtansy <261604151+springtansy@users.noreply.github.com>
2026-08-12 10:02:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -121,6 +121,35 @@ function getLegalMoves(row, col) {
 
     const moves = [];
 
+    if (piece === "wN" || piece === "bN") {
+        const directions = [[2, 1],[2, -1],[-2, 1],[-2, -1],[1, 2],[1, -2],[-1, 2],[-1, -2],];
+
+        for (const [rowDirection, colDirection] of directions) {
+            let newRow = row + rowDirection;
+            let newCol = col + colDirection;
+
+            if (
+                newRow >= 0 &&
+                newRow < 8 &&
+                newCol >= 0 &&
+                newCol < 8
+            ) {
+                const target = startingPosition[newRow][newCol];
+
+                // Empty square
+                if (!target) {
+                    moves.push([newRow, newCol]);
+                } else {
+                    if (target[0] !== piece[0]) {
+                        moves.push([newRow, newCol]);
+                    }
+                }
+            }
+        }
+
+        return moves;
+    }
+
     if (piece === "wR" || piece === "bR") {
         return getSlidingMoves(row, col, [
             [1, 0],
