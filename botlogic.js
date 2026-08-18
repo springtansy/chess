23a122f356b079257bf83413bee68e6c55a4a96a
springtansy <261604151+springtansy@users.noreply.github.com>
2026-08-18 21:56:51 +0300

    Create botlogic.js

---
+++ botlogic.js
@@ -0,0 +1,64 @@
+function makeBotMove(botName) {
+    const move = getRandomMove(currentTurn);
+
+    const {from, to, promotion} = move;
+    
+    const [fromRow, fromCol] = from;
+    const [toRow, toCol] = to;
+
+    const movingPiece = currentPosition[fromRow][fromCol];
+
+    movePiece(fromRow,fromCol,toRow,toCol,promotion)
+    renderBoard()
+    completeMove(fromRow,fromCol,toRow,toCol,movingPiece)
+}
+
+function getAllLegalMoves(color) {
+    const moves = [];
+
+    for (let pieceRow = 0; pieceRow < 8; pieceRow++) {
+        for (let pieceCol = 0; pieceCol < 8; pieceCol++) {
+            const piece = currentPosition[pieceRow][pieceCol];
+
+            if (!piece || piece[0] !== color) {
+                continue;
+            }
+
+            const pieceMoves = getLegalMoves(pieceRow, pieceCol);
+
+            for (const move of pieceMoves) {
+                const [toRow, toCol] = move;
+
+                const isPromotion =
+                    piece[1] === "P" &&
+                    (toRow === 0 || toRow === 7);
+
+                if (isPromotion) {
+                    for (const promotion of ["Q", "R", "B", "N"]) {
+                        moves.push({
+                            from: [pieceRow, pieceCol],
+                            to: [toRow, toCol],
+                            promotion: promotion
+                        });
+                    }
+                } else {
+                    moves.push({
+                        from: [pieceRow, pieceCol],
+                        to: [toRow, toCol],
+                        promotion: null
+                    });
+                }
+            }
+        }
+    }
+
+    return moves;
+}
+
+function getRandomMove(color) {
+    const moves = getAllLegalMoves(color);
+
+    const randomMove = moves[Math.floor(Math.random() * moves.length)];
+
+    return randomMove;
+}
