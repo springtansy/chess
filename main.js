4f40d04b7e7a85900df192a2ddf25653922074f0
springtansy <261604151+springtansy@users.noreply.github.com>
2026-08-18 21:58:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -778,56 +778,6 @@ function isInsufficientMaterial() {
     return false;
 }
 
-function getAllLegalMoves(color) {
-    const moves = [];
-
-    for (let pieceRow = 0; pieceRow < 8; pieceRow++) {
-        for (let pieceCol = 0; pieceCol < 8; pieceCol++) {
-            const piece = currentPosition[pieceRow][pieceCol];
-
-            if (!piece || piece[0] !== color) {
-                continue;
-            }
-
-            const pieceMoves = getLegalMoves(pieceRow, pieceCol);
-
-            for (const move of pieceMoves) {
-                const [toRow, toCol] = move;
-
-                const isPromotion =
-                    piece[1] === "P" &&
-                    (toRow === 0 || toRow === 7);
-
-                if (isPromotion) {
-                    for (const promotion of ["Q", "R", "B", "N"]) {
-                        moves.push({
-                            from: [pieceRow, pieceCol],
-                            to: [toRow, toCol],
-                            promotion: promotion
-                        });
-                    }
-                } else {
-                    moves.push({
-                        from: [pieceRow, pieceCol],
-                        to: [toRow, toCol],
-                        promotion: null
-                    });
-                }
-            }
-        }
-    }
-
-    return moves;
-}
-
-function getRandomMove(color) {
-    const moves = getAllLegalMoves(color);
-
-    const randomMove = moves[Math.floor(Math.random() * moves.length)];
-
-    return randomMove;
-}
-
 function isThreefoldRepetition() {
     const currentKey = getPositionKey();
 
@@ -1024,21 +974,6 @@ function movePiece(fromRow, fromCol, toRow, toCol, promotionPiece=null) {
     return {
         promotion: movingPiece[1] === "P" && (toRow === 0 || toRow === 7)
     };
-}
-
-function makeBotMove(botName) {
-    const move = getRandomMove(currentTurn);
-
-    const {from, to, promotion} = move;
-    
-    const [fromRow, fromCol] = from;
-    const [toRow, toCol] = to;
-
-    const movingPiece = currentPosition[fromRow][fromCol];
-
-    movePiece(fromRow,fromCol,toRow,toCol,promotion)
-    renderBoard()
-    completeMove(fromRow,fromCol,toRow,toCol,movingPiece)
 }
 
 squares.forEach((square, index) => {
